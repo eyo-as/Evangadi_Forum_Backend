4245712d3a05d58166d07a40a54111984981fb64
eyo-as <eyosiyastumiso7@gmail.com>
2024-08-16 16:04:48 +0300

    db

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,10 +1,10 @@
 const mysql2 = require("mysql2");
 
 const dbConnection = mysql2.createPool({
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  host: process.env.HOST,
-  password: process.env.PASSWORD,
+  user: "deveyomw_forum_user",
+  database: "deveyomw_forum_database",
+  host: "localhost",
+  password: "@eyo4s78DataBase",
   connectionLimit: 10,
 });
 
