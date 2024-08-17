ff565dfb27ae8039de94c735f3ce9c236df25aca
eyo-as <eyosiyastumiso7@gmail.com>
2024-08-17 12:29:50 +0300

    db

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,11 +1,21 @@
+require("dotenv").config();
 const mysql2 = require("mysql2");
 
 const dbConnection = mysql2.createPool({
-  user: "evangadi-user",
-  database: "evangadi_forum",
-  host: "localhost",
-  password: "@eyo4s78DataBase",
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
   connectionLimit: 10,
+});
+
+dbConnection.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection failed:", err.message);
+  } else {
+    console.log("Database connected successfully!");
+    connection.release();
+  }
 });
 
 module.exports = dbConnection.promise();
