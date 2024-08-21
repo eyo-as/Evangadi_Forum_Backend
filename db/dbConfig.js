158fdd56e4293be7e4ab9888f1092d7b2c7f987c
eyo-as <eyosiyastumiso7@gmail.com>
2024-08-21 15:18:28 +0300

    db

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -7,11 +7,17 @@ const dbConnection = mysql2.createPool({
   // host: process.env.DB_HOST,
   // password: process.env.DB_PASSWORD,
   // connectionLimit: 10,
+
   user: "evangadi-user",
   database: "evangadi_forum",
   host: "localhost",
   password: "@eyo4s78DataBase",
-  connectionLimit: 10,
+
+  // user: "deveyomw_evangadi-forum-user",
+  // database: "deveyomw_evangadi-forum-db",
+  // host: "109.70.148.62",
+  // password: "!q?y}.IEv9~1",
+  // connectionLimit: 10,
 });
 
 dbConnection.getConnection((err, connection) => {
