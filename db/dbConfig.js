51f1d635c1de25479e48f0aa063e0e6fb1976038
eyo-as <eyosiyastumiso7@gmail.com>
2024-08-21 11:07:23 +0300

    update

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 const mysql2 = require("mysql2");
 
 const dbConnection = mysql2.createPool({
+  // user: process.env.DB_USER,
+  // database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // password: process.env.DB_PASSWORD,
+  // connectionLimit: 10,
   user: "evangadi-user",
   database: "evangadi_forum",
   host: "localhost",
