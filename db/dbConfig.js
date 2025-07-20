6155d2d8f0ee5a17b07bf4aa1c4684d43d2b4a73
eyo-as <eyosiyastumiso7@gmail.com>
2025-07-20 21:37:00 +0300

    configure db connection

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,18 +2,12 @@ require("dotenv").config();
 const mysql2 = require("mysql2");
 
 const dbConnection = mysql2.createPool({
-  user: "deveyomw_new-user",
-  database: "deveyomw_new-db",
-  host: "109.70.148.62",
-  password: "@eyo4s78",
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
   connectionLimit: 10,
-
-  // *********************
-  // user: "evangadi-user",
-  // database: "evangadi_forum",
-  // host: "localhost",
-  // password: "@eyo4s78DataBase",
-  // connectionLimit: 10,
 });
 
 dbConnection.getConnection((err, connection) => {
